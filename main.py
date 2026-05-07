997f8038764f943281424256d5d4268c5bdc9d21
ShakeelImran <280563773+shakeelimranjbd-cloud@users.noreply.github.com>
2026-05-07 17:31:48 +0500

    1. variable

---
+++ main.py
@@ -0,0 +1,32 @@
+# name=input("Enter your name: ")
+# print("Your name is: "+name)
+# print(name+" lives in Khushab")
+
+# username=input("Enter your username: ")
+# print("your username is: "+username)
+
+# print(username+" playing a game")
+
+# game=input("what is the best game: ")
+# print("best game is: "+game)
+# print(game+" is the best game")
+
+# hobby=input("what is your hobby? ")
+# print("my hobby is: "+hobby)
+# print(hobby+" is the best hobby")
+
+# name="Shakeel"
+# city="Khushab"
+# print(name+" lives in "+city)
+
+# pet="cat"
+# live="jungle"
+# print(pet+" lives in "+live)
+# story="long"
+# name="horror"
+# print(story+" best "+name)
+
+read="novel"
+name="english"
+# print(name+" is best reading "+read)
+print("i am reading " +name +" "+read)
