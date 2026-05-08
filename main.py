9a5aade813d8860c98bf63575cc6ca158b7df3aa
ShakeelImran <280563773+shakeelimranjbd-cloud@users.noreply.github.com>
2026-05-08 18:49:12 +0500

    1. variable

--- main.py
+++ main.py
@@ -1,100 +1,102 @@
-name=input("Enter your name: ")
-print("Your name is: "+name)
-print(name+" lives in Khushab")
+# name=input("Enter your name: ")
+# print("Your name is: "+name)
+# print(name+" lives in Khushab")
+#
+# name=input("what is your good name: ")
+#
+# print("my good name is " +name)
+# print(name+" lives is isd")
+#
+# username=input("Enter your username: ")
+# print("your username is: "+username)
+#
+# print(username+" playing a game")
+#
+# game=input("what is the best game: ")
+# print("best game is: "+game)
+# print(game+" is the best game")
+#
+# hobby=input("what is your hobby? ")
+# print("my hobby is: "+hobby)
+# print(hobby+" is the best hobby")
+#
+# name="Shakeel"
+# city="Khushab"
+# print(name+" lives in "+city)
+#
+# pet="cat"
+# live="jungle"
+# print(pet+" lives in "+live)
+# story="long"
+# name="horror"
+# print(story+" best "+name)
+#
+# read="novel"
+# name="english"
+# print(name+" is best reading "+read)
+# print("i am reading " +name +" "+read)
+#
+# name=input("what is your name: ")
+# print("your name is: " + name)
+# print(name + " lives in pindi")
+#
+# name=input("what is your name: ")
+# print("my name is: " +name)
+# print(name + " lives in lahore")
+#
+# read=input("which class do you read: ")
+# print("i read in class: " +read)
+# print(read + " is my favourite")
+# print("i like "+read)
+#
+# username=input("what is your username: ")
+# print("my username is: " +username)
+# print(username+ " is best username")
+#
+# media=input("which one is your favourite media: ")
+# print("my favourite media is: " +media)
+# print(media+ " is form united states")
+#
+# name="arib"
+# city="pindi"
+# print(name+ " lives in "+city)
+#
+# story="scray"
+# read="english"
+# print("i like to read " +story+ " story in " +read)
+#
+# daughter="fatima"
+# game="polo"
+# print("favourite game of my daughter " +daughter+" is " +game)
+#
+# name=input("what is your name: ")
+# print("my name is "+name)
+# print("i live in khushab")
+#
+# user="alpha"
+# user2=input("what is your username: ")
+# # length
+# print(len(user2))
+#
+# user1="shakil"
+# user1_length=len(user1)
+# print(user1_length)
+#
+# # user2="imran"
+# # user2_length=len(user2)
+# # print(user2)
+# print(len("4555555"))
+#
+# customer1="shah ji"
+# customer1_lenght=len(customer1)
+# print(customer1_lenght)
+#
+# student="Anabia Fatima"
+# student_lenght=len(student)
+# print(student_lenght)
+#
+# Bus="volvo"
+# Bus_lenght=len(Bus)
+# print(Bus_lenght)
 
-name=input("what is your good name: ")
-
-print("my good name is " +name)
-print(name+" lives is isd")
-
-username=input("Enter your username: ")
-print("your username is: "+username)
-
-print(username+" playing a game")
-
-game=input("what is the best game: ")
-print("best game is: "+game)
-print(game+" is the best game")
-
-hobby=input("what is your hobby? ")
-print("my hobby is: "+hobby)
-print(hobby+" is the best hobby")
-
-name="Shakeel"
-city="Khushab"
-print(name+" lives in "+city)
-
-pet="cat"
-live="jungle"
-print(pet+" lives in "+live)
-story="long"
-name="horror"
-print(story+" best "+name)
-
-read="novel"
-name="english"
-print(name+" is best reading "+read)
-print("i am reading " +name +" "+read)
-
-name=input("what is your name: ")
-print("your name is: " + name)
-print(name + " lives in pindi")
-
-name=input("what is your name: ")
-print("my name is: " +name)
-print(name + " lives in lahore")
-
-read=input("which class do you read: ")
-print("i read in class: " +read)
-print(read + " is my favourite")
-print("i like "+read)
-
-username=input("what is your username: ")
-print("my username is: " +username)
-print(username+ " is best username")
-
-media=input("which one is your favourite media: ")
-print("my favourite media is: " +media)
-print(media+ " is form united states")
-
-name="arib"
-city="pindi"
-print(name+ " lives in "+city)
-
-story="scray"
-read="english"
-print("i like to read " +story+ " story in " +read)
-
-daughter="fatima"
-game="polo"
-print("favourite game of my daughter " +daughter+" is " +game)
-
-name=input("what is your name: ")
-print("my name is "+name)
-print("i live in khushab")
-
-user="alpha"
-user2=input("what is your username: ")
-# length
-print(len(user2))
-
-user1="shakil"
-user1_length=len(user1)
-print(user1_length)
-
-# user2="imran"
-# user2_length=len(user2)
-# print(user2)
-print(len("4555555"))
-
-customer1="shah ji"
-customer1_lenght=len(customer1)
-print(customer1_lenght)
-
-student="Anabia Fatima"
-student_lenght=len(student)
-print(student_lenght)
-
-Bus="volvo"
-Bus_lenght=len(Bus)
-print(Bus_lenght)
+print(len("12345"))
